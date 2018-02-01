da1cad2695e2e14f5accdef4f38da0fee8c3cfb7
Ramjivan Jangid <ramjeewanj@gmail.com>
2018-02-01 16:22:55 +0530

    removed bootstrap cdn
    it was causing problem

--- headTags.php
+++ headTags.php
@@ -4,6 +4,6 @@
 <link rel="stylesheet" href="css/style.css" type='text/css' media="all"/>
 <link rel="stylesheet" href="css/responsive.css" type='text/css' media="all"/>
 <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 <!--we can also include favicons and all the head common tags-->
 <!--Most browsers search for /favicon.ico in the website. It usually caches and will work across the whole website from the one directory.-->
