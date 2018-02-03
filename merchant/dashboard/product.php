b7beee97e9ce4c86e16e3eacd33d8569e2086bf6
Kunal Awasthi <awasthikunal.ka@gmail.com>
2018-02-03 17:58:39 +0530

    prdoduct add form prototype

--- merchant/dashboard/product.php
+++ merchant/dashboard/product.php
@@ -38,6 +38,48 @@
 					</table>
 				</div>
 			</div>
+			
+			<div class="center-block">
+				<h3>Add Product</h3>
+				<div>
+					<form class="pdct-form">
+						<div id="vs">
+							<h3 id="vsh3">Something Went wrong</h3>
+						</div>
+						<div class="form-group">
+							<input id="name" onchange="validate({'id':'name','name':'Email Address','regex':/^[a-zA-Z ]+$/,'length':null,'min_length':9,'max_length':null})" type="text" placeholder="Full Name" name="p_name"/>
+						</div>
+						<div class="form-group">
+							<textarea id="description" onchange="validate({'id':'description','name':'description','regex':null,'length':null,'min_length':9,'max_length':255})" type="text" placeholder="255 char long description" name="p_description"></textarea>
+						</div>
+						<div class="form-group">
+							<input id="price" onchange="validate({'id':'Price','name':'Price','regex':/^[0-9]+$/,'length':null,'min_length':1,'max_length':6})" type="text" placeholder="Price" name="p_price"/>
+						</div>
+						<div class="form-group">
+							<select id="category" onchange="validate({'id':'category','name':'Category','regex':/^[0-9]+$/,'length':null,'min_length':1,'max_length':null})" name="p_category">
+								<option value="<select>">Select Category</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<input id="stock" onchange="validate({'id':'stock','name':'Stock','regex':/^[0-9]+$/,'length':null,'min_length':1,'max_length':6})" type="text" placeholder="Stocks" name="p_stock"/>
+						</div>
+						<div class="form-group">
+							<input id="image1" onchange="validate({'id':'image1','name':'image1','regex':null,'length':null,'min_length':1,'max_length':null})" type="file" name="image1"/>
+						</div>
+						<div class="form-group">
+							<input id="hlgt1" onchange="validate({'id':'hlgt1','name':'Hightlight1','regex':null,'length':null,'min_length':5,'max_length':null})" type="text" placeholder="Hightlight 1" name="hlgt1"/>
+						</div>
+						<div class="spec form-group">
+							<div class="spec">
+								<h3>Specification 1</h3>
+								<input id="sp_name1" onchange="validate({'id':'sp_name1','name':'Spec Name 1','regex':null,'length':null,'min_length':5,'max_length':null})" type="text" placeholder="Spec Name" name="sp_name1"/>
+								<input id="sp_value1" onchange="validate({'id':'sp_value1','name':'Spec Value 1','regex':null,'length':null,'min_length':5,'max_length':null})" type="text" placeholder="Spec Value" name="sp_value1"/>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</form>
+				</div>
+			</div>
 		</div>
 
 	<?php
