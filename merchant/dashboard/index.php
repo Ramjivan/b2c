bd437e15a01d8f087ab7dc423c3f0cae436eb9ef
Ramjivan Jangid <ramjeewanj@gmail.com>
2018-02-09 14:08:46 +0530

    jk-radio componant

--- merchant/dashboard/index.php
+++ merchant/dashboard/index.php
@@ -18,29 +18,48 @@
 		include 'header.php';
 	?>
 	
-	<div class="container"> 
-	
-		<div class="jk-f">
-			<form action="">
-				<div class="jk-fg">
-					<input type="text" class="jk-textbox" placeholder="username" require>
-				</div>
-				<div class="jk-fg">
-					<input type="password" class="jk-textbox" placeholder="password" require>
-				</div>
-				<div class="jk-fg">
-					<input type="submit" class="btn btn-primary-color">
-				</div>
-				
-					
-			</form>
+	<div class="form-card"> 
+		some tex blal
+		<div class="radio-group">
+			<div class="jk-radio">
+				<input id="1" type="radio" name="g" checked>
+				<label for="1">Option 1</label>
+			</div>
+			<div class="jk-radio">
+				<input id="2" type="radio" name="g">
+				<label for="2">Option 2</label>
+			</div>
 		</div>
-			
-		<form>
-  <input placeholder="Username" type="text">
-  <input placeholder="Password" type="password">
-	<textarea placeholder="Description"></textarea>
-</form> 
+		
+		<div>
+    <div class="jk-radio">
+      <input id="1" type="radio" name="g" checked>
+    </div>
+    <div class="jk-radio">
+      <input id="2" type="radio" name="g">
+      <label for="2">Option 2</label>
+    </div>
+  </div>
+  <div>
+    <div class="jk-radio jk-radio-inline">
+      <input id="3" type="radio" name="g2" checked>
+      <label for="3">Option 3</label>
+    </div>
+    <div class="jk-radio jk-radio-inline">
+      <input id="4" type="radio" name="g2">
+      <label for="4">Option 4</label>
+    </div>
+  </div>
+  <div>
+    <div class="jk-radio jk-radio-inline">
+      <input id="5" type="radio" name="g3" checked>
+      <label for="5"></label>
+    </div>
+    <div class="jk-radio jk-radio-inline">
+      <input id="6" type="radio" name="g3">
+      <label for="6">Option 6</label>
+    </div>
+  </div>
 	</div>
 
 	<?php
