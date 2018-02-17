1d90562a94fd1bf200941348ce4aef5088ffe42b
Kunal Awasthi <awasthikunal.ka@gmail.com>
2018-02-17 19:06:49 +0530

    product page UI complete

--- product.php
+++ product.php
@@ -26,7 +26,21 @@ include('sess_val.php');
 	<!--Content Here-->
     <div class="main-container" >
     </div>
-
+<div class="dialog" id="dg1">
+	<div id="dhead">
+	Ask Question
+	<span class="fa right" onclick="this.parentElement.parentElement.style.display='none';document.getElementsByClassName('blurdfg')[0].style.display='none';">
+		&times;
+		</span>
+		<div class="clearfix"></div>
+	</div>
+	<div class="dialog-body">
+		<form class="jk-form">
+			<textarea name="qna_question" placeholder="Ask Question(255 char allowed)"></textarea>
+			<input type="button" id="q_a_btn" value="Ask" class="btn btn-success btn-sm" style="margin:2px;"></input>
+		</form>
+	</div>
+</div>
 	<?php
 		//including footer
 		include 'footer.php';
