f2aea799f328b2457efb283a9f4d63d09a504add
jivan <ramjeewanj@gmail.com>
2018-03-08 15:17:29 +0530

    test commit to check aws codepipline working or not

--- headTags.php
+++ headTags.php
@@ -1,4 +1,4 @@
-<base href="/b2c/"/>
+<base href="/"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script language="JavaScript" type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 <script language="JavaScript" type="text/javascript" src="js/style.js"></script>
