1bdfbde59652fcf8b4fd9644c01effc5ac246eee
Kunal Awasthi <awasthikunal.ka@gmail.com>
2018-03-15 11:26:46 +0530

    Update headTags.php

--- merchant/dashboard/headTags.php
+++ merchant/dashboard/headTags.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="/css/style.css" type='text/css' media="all"/>
 <link rel="stylesheet" href="/css/style2.css" type='text/css' media="all"/>
 <link rel="stylesheet" href="/css/responsive.css" type='text/css' media="all"/>
-<link rel="stylesheet" href="/css/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css">
  <base href="/merchant/dashboard/"/>
 <!--we can also include favicons and all the head common tags-->
 <!--Most browsers search for /favicon.ico in the website. It usually caches and will work across the whole website from the one directory.-->
