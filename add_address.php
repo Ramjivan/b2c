764ee0191e4f69bfb0587864bd1fc3eebbdd4f9b
Kunal Awasthi <awasthikunal.ka@gmail.com>
2018-03-15 17:22:38 +0530

    b2c fix

--- add_address.php
+++ add_address.php
@@ -7,7 +7,7 @@ include('sess_val.php');
 		<title>
 			Customer Address | B2C
 		</title>
-		<base href="/b2c/">
+		<base href="/">
 		<meta name="description" content="">
 		<meta name="keywords" content="">
 		<script src="js/_add_adr_ctmr.js" language="JavaScript" type="text/javascript"></script>
