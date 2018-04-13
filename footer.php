4df745bbb70b45899972818a57daea7e10e63bde
KunalAwasthi <awasthikunal.ka@gmail.com>
2018-04-13 14:14:07 +0530

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,39 @@
 <div class="btt">
         <span class="fa fa-arrow-up"></span>
 </div>
-
 <footer>
-    <a href="/merchant/dashboard">Merchant Dashboard</a>
+    <div class="row">
+        
+        <div class="col-2 clearfix">
+            <div class="quick-links">
+                <h3>Follow us</h3>
+                <ul>
+                    <li><a href="#"><i class="fa fa-2x fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-2x fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa fa-2x fa-whatsapp"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+         <div class="col-2 clearfix">
+            <div class="quick-links">
+                <h3>Sell on b2c</h3>
+                <ul>
+                    <li><a href="/merchant/dashboard">start Selling</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-2 clearfix">
+            <div class="quick-links">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/cutomer/dashboard">Dashboard</a></li>
+                    <li><a href="/customer/orders">orders</a></li>
+                </ul>
+            </div>
+        </div>
+    
+    </div>
 </footer>
